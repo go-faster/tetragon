8d1af2554fa1271c938be4fdd86df74929fdcffb
Aleksandr Razumov <ernado@ya.ru>
2023-02-21 14:21:16 +0300

    vendor: update cilium/tetragon to go-faster/tetragon

--- go.mod
+++ go.mod
@@ -139,8 +139,8 @@ require (
 // has to be in sync with both cilium and hubble overrides (mostly cilium).
 replace (
 	// Use local version of API
-	github.com/go-faster/tetragon/api => ./api
-	github.com/go-faster/tetragon/pkg/k8s => ./pkg/k8s
+	github.com/go-faster/tetragon/api v0.8.6 => ./api
+	github.com/go-faster/tetragon/pkg/k8s v0.8.6 => ./pkg/k8s
 
 	github.com/miekg/dns => github.com/cilium/dns v1.1.4-0.20190417235132-8e25ec9a0ff3
 	github.com/optiopay/kafka => github.com/cilium/kafka v0.0.0-20180809090225-01ce283b732b
--- vendor.sh
+++ vendor.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+VERSION=$1
+
 grep -rl cilium/tetragon --exclude-dir=vendor --exclude-dir=.git --exclude vendor.sh | xargs sed -i 's|cilium/tetragon|go-faster/tetragon|g'
-grep -rl v0.0.0-00010101000000-000000000000 --exclude-dir=vendor --exclude-dir=.git --exclude vendor.sh | xargs sed -i 's|v0.0.0-00010101000000-000000000000|v0.8.6|g'
+grep -rl v0.0.0-00010101000000-000000000000 --exclude-dir=vendor --exclude-dir=.git --exclude vendor.sh | xargs sed -i "s|v0.0.0-00010101000000-000000000000|$1|g"
 
 make vendor
 
 git add vendor api pkg
 git commit -a -m "vendor: update cilium/tetragon to go-faster/tetragon"
+
+git tag -a $VERSION -m "v$VERSION"
+make image VERSION=$VERSION DOCKER_IMAGE_TAG=$VERSION
