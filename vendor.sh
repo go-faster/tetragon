85cf7b8d3729ba68fd67f5ab675ec208c1191b0c
Aleksandr Razumov <ernado@ya.ru>
2023-02-21 14:39:21 +0300

    fix: vendor

--- vendor.sh
+++ vendor.sh
@@ -3,11 +3,11 @@
 VERSION=$1
 
 grep -rl cilium/tetragon --exclude-dir=vendor --exclude-dir=.git --exclude vendor.sh | xargs sed -i 's|cilium/tetragon|go-faster/tetragon|g'
-grep -rl quay.io --exclude-dir=vendor --exclude-dir=.git --exclude vendor.sh | xargs sed -i 's|quay.io|ghcr.io|g'
+sed -i 's|quay.io|ghcr.io|g' install/kubernetes/values.yaml
 sed -i 's|-t "go-faster|-t "ghcr.io/go-faster|g' Makefile
 sed -i 's|push go-faster|push ghcr.io/go-faster|g' Makefile
 grep -rl v0.0.0-00010101000000-000000000000 --exclude-dir=vendor --exclude-dir=.git --exclude vendor.sh | xargs sed -i "s|v0.0.0-00010101000000-000000000000|$1|g"
-grep -rl v0.8.3 --exclude-dir=vendor --exclude-dir=.git --exclude vendor.sh | xargs sed -i "s|v0.8.3|$1|g"
+sed -i "s|v0.8.3|$1|g" install/kubernetes/values.yaml
 
 make vendor
 
